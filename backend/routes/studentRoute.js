345b263fb08e2f78d1a8468321afc7d93daa1e88
shefa-ul123 <shefaul810@gmail.com>
2026-05-01 10:51:27 +0530

    Student route updated

--- backend/routes/studentRoute.js
+++ backend/routes/studentRoute.js
@@ -25,3 +25,45 @@ router.post("/register-face", protect, studentOnly, registerFace);
 router.post("/verify-face", protect, studentOnly, verifyStudentFace);
 
 export default router;
+
+import express from "express";
+import {
+  registerStudent,
+  loginStudent,
+  verifyStudentEmail,
+  forgotStudentPassword,
+  resetStudentPassword,
+  getStudentProfile,
+  updateStudentProfile,
+  changeStudentPassword,
+  registerFace,
+  verifyStudentFace,
+  sendPasswordResetOTP,
+  verifyPasswordResetOTP,
+} from "../controllers/studentController.js";
+
+import {
+  protect,
+  studentOnly,
+} from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// Public Routes
+router.post("/register", registerStudent);
+router.post("/login", loginStudent);
+router.get("/verify-email", verifyStudentEmail);
+router.post("/forgot-password", forgotStudentPassword);
+router.post("/reset-password", resetStudentPassword);
+
+
+// Student Protected Routes
+router.post("/send-otp", sendPasswordResetOTP);
+router.post("/verify-otp", verifyPasswordResetOTP);
+router.get("/profile", protect, studentOnly, getStudentProfile);
+router.put("/profile", protect, studentOnly, updateStudentProfile);
+router.put("/change-password", protect, studentOnly, changeStudentPassword);
+router.post("/register-face", protect, studentOnly, registerFace);
+router.post("/verify-face", protect, studentOnly, verifyStudentFace);
+
+export default router;
